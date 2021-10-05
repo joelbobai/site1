152acdf0ee8618274802eb91703ca1d0e484fe52
joelbobai <90658305+joelbobai@users.noreply.github.com>
2021-10-05 16:25:23 +0100

    Add files via upload

---
+++ skm.js
@@ -0,0 +1,14 @@
+alert("Hi, am Joel")
+var yourname = prompt("What Is Your Name");
+alert("You Have A Nice Name, " + yourname );
+alert(" Welcome To My Site");
+alert("Please leave a comment");
+
+new Masonry(".-post .grid",{
+	itemSelector:'.grid-item',
+	gutter:20
+
+});
+
+
+
