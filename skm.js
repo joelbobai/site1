b5e1b7b34134c8ddf20aceacdf14f0bc65be00dd
joelbobai <90658305+joelbobai@users.noreply.github.com>
2021-10-07 23:12:48 +0100

    Update skm.js

--- skm.js
+++ skm.js
@@ -1,6 +1,10 @@
 alert("Hi, am Joel")
 var yourname = prompt("What Is Your Name");
-alert("You Have A Nice Name, " + yourname );
+var namef = yourname.slice(0,1);
+var namew = namef.toUpperCase();
+var namee =  yourname.slice(1,30);
+var namer = namee.toLowerCase();
+alert("You Have A Nice Name, " +namew+namer);
 alert(" Welcome To My Site");
 alert("Please leave a comment");
 
